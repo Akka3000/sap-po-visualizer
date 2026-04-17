0cc89eee7f95af04ce1b1c6d7f23e5bdd8bfcb40
Akka3000 <almir762liu@gmail.com>
2026-04-17 23:42:01 +0200

    Refactor app.py to restore original functionality

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ fig = px.timeline(df, x_start="Start", x_end="Slut", y="Fas", color="Status",
                  color_discrete_map={
                      "Nu-Period": "#008000", 
                      "Låst tid": "#Ff0000", 
-                     "Produktion": "#0068C9"
+                     "Produktion": "#0000ff"
                  })
 
 # Fixa utseendet
